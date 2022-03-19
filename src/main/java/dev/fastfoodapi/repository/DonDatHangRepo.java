cdace7a830a2d6bb2432b729467b51b0c52adc27
MSI\Minh Tan <tanlun04112000@gmail.com>
2022-03-19 12:31:01 +0700

    render data chart

--- src/main/java/dev/fastfoodapi/repository/DonDatHangRepo.java
+++ src/main/java/dev/fastfoodapi/repository/DonDatHangRepo.java
@@ -17,21 +17,21 @@ public interface DonDatHangRepo extends JpaRepository<DonDatHang, Long> {
 
     @Query(value = "SELECT maddh, SUM(tong_tien) AS tong_tien, ngay_dat_hang, dia_chi_giao_hang, trang_thai, ma_khach_hang, hinh_thuc\n" +
             "FROM db_fastfood.tbl_dondathang\n" +
-            "WHERE MONTH(ngay_dat_hang) = MONTH(NOW())\n" +
+            "WHERE MONTH(ngay_dat_hang) = MONTH(NOW()) AND trang_thai = 'Đã thanh toán'\n" +
             "GROUP BY DAY(ngay_dat_hang)\n" +
             "ORDER BY DAY(ngay_dat_hang) ASC", nativeQuery = true)
     List<DonDatHang> ngayTheoThang();
 
     @Query(value = "SELECT maddh, SUM(tong_tien) AS tong_tien, ngay_dat_hang, dia_chi_giao_hang, trang_thai, ma_khach_hang, hinh_thuc\n" +
             "FROM db_fastfood.tbl_dondathang\n" +
-            "WHERE YEAR(ngay_dat_hang) = YEAR(NOW())\n" +
+            "WHERE YEAR(ngay_dat_hang) = YEAR(NOW()) AND trang_thai = 'Đã thanh toán'\n" +
             "GROUP BY MONTH(ngay_dat_hang)\n" +
             "ORDER BY MONTH(ngay_dat_hang) ASC", nativeQuery = true)
     List<DonDatHang> thangTrongNam();
 
     @Query(value = "SELECT maddh, SUM(tong_tien) AS tong_tien, ngay_dat_hang, dia_chi_giao_hang, trang_thai, ma_khach_hang, hinh_thuc\n" +
             "FROM db_fastfood.tbl_dondathang\n" +
-            "WHERE MONTH(ngay_dat_hang) = MONTH(NOW()) AND ngay_dat_hang >= DATE_SUB(NOW(), INTERVAL 7 DAY)\n" +
+            "WHERE MONTH(ngay_dat_hang) = MONTH(NOW()) AND ngay_dat_hang >= DATE_SUB(NOW(), INTERVAL 7 DAY) AND trang_thai = 'Đã thanh toán'\n" +
             "GROUP BY DAY(ngay_dat_hang)\n" +
             "ORDER BY DAY(ngay_dat_hang) ASC", nativeQuery = true)
     List<DonDatHang> bayNgayGanDay();
